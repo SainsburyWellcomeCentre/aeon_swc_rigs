4c59d476021a971d977640c59e15a175f6458180
glopesdev <g.lopes@neurogears.org>
2025-11-03 10:27:02 +0000

    Add base experiment schema

---
+++ src/swc/aeon_rigs/experiment.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from swc.aeon_rigs.base import BaseSchema
+from swc.aeon_rigs.rig import Rig
+
+
+class Experiment(BaseSchema):
+    workflow: str = Field(description="Path to the workflow running the experiment.")
+    commit: str = Field(description="Commit hash of the experiment repo.")
+    repository_url: str = Field(
+        description="The URL of the git repository used to version experiment source code."
+    )
+    rig: Rig
