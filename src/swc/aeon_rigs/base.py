fae571dd87da7cb0d89cea6e2d3de4611b2a3f47
glopesdev <g.lopes@neurogears.org>
2025-11-04 12:28:12 +0000

    Allow arbitrary types in base schema
    This is to allow read-only computed properties to return data reader
    types.

--- src/swc/aeon_rigs/base.py
+++ src/swc/aeon_rigs/base.py
@@ -6,6 +6,7 @@ from pydantic.alias_generators import to_camel, to_pascal
 class BaseSchema(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_camel,
+        arbitrary_types_allowed=True,
         field_title_generator=lambda n, _: to_pascal(n),
         populate_by_name=True,
         from_attributes=True,
