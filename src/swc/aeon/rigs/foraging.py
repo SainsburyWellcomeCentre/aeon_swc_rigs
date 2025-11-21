32a6e206a8e7a3a05cf856ea940878e4fdcbb4d1
glopesdev <g.lopes@neurogears.org>
2025-11-21 18:14:25 +0000

    Add feeder command to foraging module (#7)
    This reflects the current subject API exposed by the UndergroundFeeder
    workflow.

--- src/swc/aeon/rigs/foraging.py
+++ src/swc/aeon/rigs/foraging.py
@@ -1,8 +1,12 @@
+from enum import StrEnum
 from pydantic import Field
+from swc.aeon.rigs.base import BaseSchema
 from swc.aeon.rigs.harp import HarpOutputExpander
 
 
 class UndergroundFeeder(HarpOutputExpander):
+    """Configures control and acquisition functionality for an underground feeder module."""
+
     pellet_delivery_retry_count: int = Field(
         default=2,
         ge=0,
@@ -14,3 +18,18 @@ class UndergroundFeeder(HarpOutputExpander):
         description="The amount of time to wait for pellet detection before reporting a failure.",
     )
     wheel_radius: float = Field(default=-4.0, description="The radius of the wheel, in centimeters.")
+
+
+class FeederCommand(StrEnum):
+    """Specifies the type of command to send to an underground feeder controller."""
+
+    DELIVER_PELLET = "DeliverPellet"
+    RESET_FEEDER = "ResetFeeder"
+
+
+class CreateFeederCommand(BaseSchema):
+    """Creates a command to send to an underground feeder controller."""
+
+    command: FeederCommand = Field(
+        default=FeederCommand.DELIVER_PELLET, description="The command to send to the feeder device."
+    )
