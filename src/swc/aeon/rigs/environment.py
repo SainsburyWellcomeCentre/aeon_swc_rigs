154049b0dfc9d11fe9e9d807e547c3d2ad5576e9
glopesdev <g.lopes@neurogears.org>
2025-11-21 18:20:34 +0000

    Add new environment module (#8)
    * Add light controller to environment module
    
    This model represents both the light client and light cycle attributes
    used for configuring the light controller in aeon rigs.
    
    * Add weight scale to environment module
    
    This model represents acquisition configuration for automated habitat
    weighing devices.

---
+++ src/swc/aeon/rigs/environment.py
@@ -0,0 +1,35 @@
+from pydantic import Field
+from swc.aeon.rigs.base import BaseSchema, Device
+
+
+class LightController(BaseSchema):
+    """Configures a network client for automated room light control."""
+
+    event_socket: str = Field(
+        default=">tcp://localhost:4303",
+        description="Specifies the endpoint to send commands to the Light Server.",
+    )
+    command_socket: str = Field(
+        default=">tcp://localhost:4304",
+        description="Specifies the endpoint to send commands to the Light Server.",
+    )
+    room_name: str = Field(description="The name of the room to monitor and control.")
+    config_file_name: str = Field(
+        default="lightcycle.config",
+        description="The name of the CSV file describing the light model, "
+        "where each row represents one whole minute and the red, cold white"
+        "and warm white, light levels set for that minute.",
+    )
+
+
+class WeightScale(Device):
+    """Configures acquisition functionality for automated habitat weighing scales."""
+
+    port_name: str = Field(examples=["COM"], description="The name of the device serial port.")
+    filter_window: int = Field(
+        default=40, description="Sliding window size of the weight linear regression filter."
+    )
+    weight_baseline_refactory_period: float = Field(
+        default=5,
+        description="The time between consecutive weight baseline when subject in center of habitat in seconds.",
+    )
